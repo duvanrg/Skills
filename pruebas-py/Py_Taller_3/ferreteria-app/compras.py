c10b837d487503423e8d5cbd396df38680c19ca5
dubanrg <135860225+duvanrg@users.noreply.github.com>
2023-07-11 10:35:26 -0500

    clase 10 Julio

--- pruebas-py/Py_Taller_3/ferreteria-app/compras.py
+++ pruebas-py/Py_Taller_3/ferreteria-app/compras.py
@@ -30,36 +30,58 @@ def MainMenu():
     print("|{:^57}|".format('ADMINISTRACION DE COMPRAS'))
     print('+','-'*55,'+')
 
-    print("1. Registrar")
-    print("2. Buscar ")
-    print("3. Devolucion")
+    print("1. Registrar compra")
+    print("2. Buscar compra")
+    print("3. Devolucion compra")
     print("4. Anular Factura compra")
     print("5. Regresar menu principal")
     opcion =int(input("> "))
     if (opcion == 1):
         data = {
-            "Id" : input("Ingrese el id del compra: "),
-            "Nombre" : input("Nombre del compra: "),
-            "Precio" : input("precio de la compra: ")
+            "Cod" : input("Ingrese el Cod del compra: "),
+            "Fecha" : input("Fecha de la compra: "),
+            "CodProd" : input("Ingrese el Cod del Producto: "),
+            "Valor" : input("Valor de la compra: "),
+            "Cantidad" : input("Cantidad comprada: ")
         }
         core.crearInfo("compras.json", data)
     elif (opcion == 2):
         os.system("clear")
         print('+','-'*55,'+')
-        print("|{:^57}|".format('BUSCADOR DE CLIENTES'))
+        print("|{:^57}|".format('BUSCADOR DE COMPRAS'))
         print('+','-'*55,'+')
-        compraSearch = input("Ingrese el codigo del cliente a buscar: ")
+        compraSearch = input("Ingrese el codigo del compra a buscar: ")
         for i, item in enumerate(diccCompras["data"]):
-            if (compraSearch in item["Id"]):
-                print(f'Id compra : {item["Id"]}')
-                print(f'Nombre compra : {item["Nombre"]}')
-                print(f'Precio compra : {item["Precio"]}')
+            if (compraSearch in item["Cod"]):
+                print(f'Codigo compra : {item["Cod"]}')
+                print(f'Fecha compra : {item["Fecha"]}')
+                print(f'Valor compra : {item["Valor"]}')
+                print(f'Cantidad compra : {item["Cantidad"]}')
     elif (opcion == 3):
-        pass
+        os.system("clear")
+        print('+','-'*55,'+')
+        print("|{:^57}|".format('DEVOLUCION DE COMPRAS'))
+        print('+','-'*55,'+')
+        compraSearch = input("Ingrese el codigo del compra a devolver: ")
+        for i, item in enumerate(diccCompras["data"]):
+            if (compraSearch in item["Cod"]):
+                diccCompras["data"][i]["Devolucion"] = True
+                print(diccCompras["data"][i])
+                core.EditarData("compras.json", diccCompras)
     elif (opcion == 4):
-        pass
+        os.system("clear")
+        print('+','-'*55,'+')
+        print("|{:^57}|".format('ANULACION COMPRAS'))
+        print('+','-'*55,'+')
+        compraSearch = input("Ingrese el codigo del compra a devolver: ")
+        for i, item in enumerate(diccCompras["data"]):
+            if (compraSearch in item["Cod"]):
+                diccCompras["data"][i]["Anulacion"] = True
+                print(diccCompras["data"][i])
+                core.EditarData("compras.json", diccCompras)
     elif (opcion == 5):
         isCliRun = False
+    os.system("sleep 5")
     if (isCliRun):
         MainMenu()
     
--- pruebas-py/Py_Taller_3/ferreteria-app/ventas.py
+++ pruebas-py/Py_Taller_3/ferreteria-app/ventas.py
@@ -1,13 +1,32 @@
 import core
 import os
 
+diccVentas = {"data":[]}
+diccProductos = {"data": []}
+
 def LoadInfoVentas(*args):
-    return core.LoadInfo("ventas.json")
+    global diccVentas
+    if (core.checkFile(args[0])):
+        return core.LoadInfo(args[0])
+    else:
+        core.crearInfo(args[0], diccVentas)
+        return core.LoadInfo(args[0])
+    
+def LoadInfoProductos(*args):
+    global diccProductos
+    if (core.checkFile(args[0])):
+        return core.LoadInfo(args[0])
+    else:
+        core.crearInfo(args[0], diccProductos)
+        return core.LoadInfo(args[0])
 
 def MainMenu():
     os.system("clear")
     isCliRun = True
     os.system("clear")
+    diccVentas = LoadInfoVentas("ventas.json")
+    diccProductos = LoadInfoProductos("productos.json")
+
     print('+','-'*55,'+')
     print("|{:^57}|".format('ADMINISTRACION DE VENTAS'))
     print('+','-'*55,'+')
@@ -19,20 +38,52 @@ def MainMenu():
     print("5. Regresar menu principal")
     opcion =int(input("> "))
     if (opcion == 1):
+        buy = 1
         data = {
-            "Id" : input("Ingrese el id del venta: "),
-            "Nombre" : input("Nombre del venta: "),
-            "Precio" : input("precio de la venta: ")
+            "Cod" : input("Ingrese el Cod del venta: "),
+            "Fecha" : input("Fecha de la venta: "),
+            "IdCli" : input("Ingrese el Id del Cliente: "),
+            "NombreCli" : input("Ingrese el nombre del Cliente: "),
+            "Total" : 0,
+            "DetalleVenta" : []
         }
-        core.crearInfo("compras.json", data)
+        while buy == 1:
+            cod = input("Ingrese el codigo del producto a vender: ")
+            cantidad = int(input("Ingrese la cantidad vendida: "))
+            valorUni = int(input("Ingrese el valor por unidad del producto: "))
+            venta = {"Codigo": cod, "Cantidad": cantidad, "ValorUnitario": valorUni}
+            data["DetalleVenta"].append(venta)
+            print("Desea vender otro producto \n1.Si \n2.No ")
+            buy = int(input("> "))
+        for i, item in enumerate(data["DetalleVenta"]):
+            
+            data["Total"] += item["Cantidad"] * item["ValorUnitario"]
+        core.crearInfo("ventas.json", data)
     elif (opcion == 2):
-        pass
+        os.system("clear")
+        print('+','-'*55,'+')
+        print("|{:^57}|".format('BUSCADOR DE VENTAS'))
+        print('+','-'*55,'+')
+        ventaSearch = input("Ingrese el codigo del venta a buscar: ")
+        for i, item in enumerate(diccVentas["data"]):
+            if (ventaSearch in item["Cod"]):
+                print(f'Codigo venta : {item["Cod"]}')
+                print(f'Fecha venta : {item["Fecha"]}')
+                print(f'ID Cliente : {item["IdCli"]}')
+                print(f'Nombre Cliente : {item["NombreCli"]}')
+                print("------------Productos------------")
+            for vent in item["DetalleVenta"]:
+                print(f'Codigo producto : {vent["Codigo"]}')
+                print(f'Cantidad producto : {vent["Cantidad"]}')
+                print(f'Valor unitario producto : {vent["ValorUnitario"]}')
+            print(f'Total venta : {item["Total"]}')
     elif (opcion == 3):
         pass
     elif (opcion == 4):
         pass
     elif (opcion == 5):
         isCliRun = False
+    os.system("sleep 5")    
     if (isCliRun):
         MainMenu()
 
